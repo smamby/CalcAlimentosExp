dcdd74e7f79f7849a36a5cc5f98927bfa5d50a27
Seba Mamby <s.mamby@gmail.com>
2022-08-11 19:23:08 -0300

    commit objeto terminada. Falta imprimir en papel para pasar a la v2

--- compilarLista.js
+++ compilarLista.js
@@ -9,12 +9,12 @@ function concatenar(){
      listaConcatenada = ingredientesDeCenas.concat(ingredientesDeDesayuno.concat(ingredientesDeMarchas));
 }
 
-var almacen = [];
-var verduleria = [];
-var dietetica = [];
-var carniceria = [];
-var kiosco = [];
-var otros = [];
+var Almacen = [];
+var Verduleria = [];
+var Dietetica = [];
+var Carniceria = [];
+var Kiosco = [];
+var Otros = [];
 
 
 var clases = [];
@@ -36,14 +36,123 @@ function separate(lista) {
     for (var categoria of clases){
         for (var i of lista){
             if(i.clasificacion.toLowerCase() === categoria.toLowerCase()){
-                 window[categoria.toLowerCase()].push(i);
+                 window[categoria].push(i);
             };
          };
     };    
 };
 
-console.table("almacen",almacen);
-console.table("verduleria",verduleria);
-console.table("dietetica",dietetica);
-console.table("kiosco",kiosco);
-console.table("otros",otros);
+var items = [];
+function encontrarItems(lista) {
+    var itemsList = [];
+    itemsList = lista.map(el=> el.ingrediente);
+    console.log(itemsList);
+    itemsList.forEach(item => {
+        if(!items.includes(item)){
+            items.push(item);
+        };
+    });
+    console.log(items);
+};
+var listaFinal = [];
+var ingredienteFinal = [];
+function sumarItemsIguales(lista){
+    for (var item of items){
+        for(var elemento of lista) {
+            if(item === elemento.ingrediente){
+                if(!ingredienteFinal.includes(item)){
+                    ingredienteFinal.push(item);
+                    listaFinal.push(elemento);
+                } else {
+                    listaFinal[listaFinal.length-1].subtotal += elemento.subtotal;
+                };
+            };
+        };   
+    };
+};
+var listaFinalMinima = [];
+function minimizarLista(lista){
+    for(var elem of lista){
+        var elementoMinimo = {
+            ingrediente : elem.ingrediente,
+            clasificacion: elem.clasificacion,        
+            unidad : elem.unidad,        
+            subtotal : Math.ceil(elem.subtotal),
+        };
+        listaFinalMinima.push(elementoMinimo);
+    };
+};
+function sumarIngredientes(){
+    concatenar()
+    encontrarItems(listaConcatenada)
+    sumarItemsIguales(listaConcatenada)
+    console.log(listaFinal)
+    minimizarLista(listaFinal);
+};
+function alistarParaImprimir(){
+    sumarIngredientes();
+    separate(listaFinalMinima);
+    console.table(Almacen);
+    console.table(Verduleria);
+    console.table(Dietetica);
+    console.table(Kiosco);
+    console.table(Otros);
+};
+//var clasesLowerCase = [] ;
+
+function CrearListas(clases){
+    //clasesLowerCase = clases.map(el=> el.toLowerCase());
+    for (var clase of clases){
+        var table = document.createElement("table");
+        table.className=`tableCompra${clase}`;
+        table.id=`tableCompra${clase}`;
+        var thead = document.createElement("thead");
+        table.appendChild(thead);
+        var tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+        var rowCompra = document.createElement("tr");
+        rowCompra.className=`rowCompra${clase}`;
+        rowCompra.id=`rowCompra${clase}`;
+        var contListasCompra = document.getElementById("contListasCompra");
+        contListasCompra.append(table);
+        var titleTable = document.createElement("div")
+        thead.appendChild(titleTable);
+        titleTable.append(clase)
+        thead.appendChild(rowCompra);
+        var tdClasific = document.createElement("td");
+        rowCompra.appendChild(tdClasific)
+        tdClasific.append("Clasificacion");        
+        var tdIngrediente = document.createElement("td");
+        rowCompra.appendChild(tdIngrediente)
+        tdIngrediente.append("Ingrediente");
+        var tdSubtotal = document.createElement("td");
+        rowCompra.appendChild(tdSubtotal);
+        tdSubtotal.append("Cantidad");
+        var tdUnidad = document.createElement("td");
+        rowCompra.appendChild(tdUnidad);
+        tdUnidad.append("Unidades");
+    
+        //for (var clase of clasesLowerCase){
+            for(var elem of window[clase]){
+                var row = tbody.insertRow(); 
+                var clasificTC = row.insertCell(0);
+                var ingredTC =  row.insertCell(1);
+                var subtotalTC = row.insertCell(2);
+                var unidadTC = row.insertCell(3);
+                clasificTC.append(elem.clasificacion);
+                ingredTC.append(elem.ingrediente);
+                subtotalTC.append(elem.subtotal);
+                unidadTC.append(elem.unidad);
+            }
+        //}
+    }       
+}
+
+function listasDeCompras(){
+    alistarParaImprimir();
+    CrearListas(clases)
+}
+
+function imprimirListasDeCompras(){
+    windows.print()
+}
